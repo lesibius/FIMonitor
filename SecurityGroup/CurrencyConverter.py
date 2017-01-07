a733b9a2f82cb36c66985f6ce3178b92375d6cd5
lesibius <clementin.castellano@gmail.com>
2017-01-07 22:38:26 +0100

    Modified CurrencyConverter to allows converting market values

--- SecurityGroup/CurrencyConverter.py
+++ SecurityGroup/CurrencyConverter.py
@@ -14,16 +14,66 @@ class CurrencyConverter:
         self.ConversionTable = {}
         
     def AddCurrency(self,iso):
+        """
+        Add a currency to the CurrencyConverter instance
+        
+        Parameters
+        ----------
+        iso: str
+            ISO code of the currency
+            
+        Returns:
+        --------
+        None
+        """
         if not iso in self.Currencies:
             self.Currencies.add(iso)
-            if len(self.Currencies) > 0:
+            if self.Currencies:
                 for c in self.Currencies:
                     self.ConversionTable[(iso,c)]=1
                     self.ConversionTable[(c,iso)]=1
     
-    def SetExchangeRate(self,iso1,iso2,rate):
-        if iso1 in self.Currencies and iso1 in self.Currencies:
-            self.ConversionTable[(iso1,iso2)]=rate
-            self.ConversionTable[(iso2,iso1)]=1/rate
+    def SetExchangeRate(self,base,quotation,rate):
+        """
+        Set the exchange rate for a currency pair. It also sets the exchange rate for the
+        reversed pair.
+        
+        Parameters
+        ----------
+        base: str
+            ISO code of the base currency
+        quotation: str
+            ISO code of the quotation currency
+        rate: float
+            Exchange rate for the currency pair
+        
+        Returns
+        -------
+        None
+        """
+        if base in self.Currencies and quotation in self.Currencies:
+            self.ConversionTable[(base,quotation)]=rate
+            self.ConversionTable[(quotation,base)]=1/rate
         else:
-            raise Exception
+            raise KeyError("One of the currencies has not been found")
+            
+    def Convert(self,base,quotation,value):
+        """
+        Convert a value expressed in the base currency to a value expressed in the 
+        quotation currency
+        
+        Parameters
+        ----------
+        base: str
+            ISO code of the base currency
+        quotation: str
+            ISO code of the quotation currency
+        value: float
+            Value expressed in the base currency to convert to the base currency
+            
+        Returns
+        -------
+        type: float
+            Value converted to the quotation currency
+        """
+        return value * self.ConversionTable[(base,quotation)]
